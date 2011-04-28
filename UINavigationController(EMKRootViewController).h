c52b2d5d7d7928da26452bb1e348f516d639b3f9
Benedict Cohen <ben@benedictcohen.co.uk>
2011-04-28 21:40:59 +0100

    - Added UINavigationController (EMKRootViewController) which contains 1 method: 	-(UIViewController *)EMK_rootViewController;

---
+++ UINavigationController(EMKRootViewController).h
@@ -0,0 +1,16 @@
+//
+//  UINavigationController(EMKRootViewController).h
+//  Jot
+//
+//  Created by Benedict Cohen on 27/04/2011.
+//  Copyright 2011 Electric Muffin Kitchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface UINavigationController (EMKRootViewController)
+
+-(UIViewController *)EMK_rootViewController;
+
+@end
---
+++ UINavigationController(EMKRootViewController).m
@@ -0,0 +1,20 @@
+//
+//  UINavigationController(EMKRootViewController).m
+//  Jot
+//
+//  Created by Benedict Cohen on 27/04/2011.
+//  Copyright 2011 Electric Muffin Kitchen. All rights reserved.
+//
+
+#import "UINavigationController(EMKRootViewController).h"
+
+
+@implementation UINavigationController (EMKRootViewController)
+
+-(UIViewController *)EMK_rootViewController
+{
+    NSArray *viewControllers = self.viewControllers;
+    return ([viewControllers count]) ? [viewControllers objectAtIndex:0] : nil;
+}
+
+@end
