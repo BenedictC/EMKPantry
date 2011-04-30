871846711ae2cef6693a9605c7eb933d3a6c0ab8
Benedict Cohen <ben@benedictcohen.co.uk>
2011-04-30 15:54:43 +0100

    - Refactored work of creating types string to function that accept valist - Added +(NSMethodSignature *)EMK_signatureForMethodWithReturnType:(const char *)returnType argumentTypes:(const char *)firstArgumentType, ...;

--- Classes/Foundation/NSMethodSignature(EMKMethodTypeEncoding).m
+++ Classes/Foundation/NSMethodSignature(EMKMethodTypeEncoding).m
@@ -9,19 +9,50 @@
 #import "NSMethodSignature(EMKMethodTypeEncoding).h"
 
 
+
+const char *EMK_NSMethodSignature_typesForMethodWithReturnType(const char *returnType, const char *firstArgumentType, va_list arguments);
+
+
+
 @implementation NSMethodSignature (EMKMethodTypeEncoding)
 
++(const char *)EMK_typesForMethodWithReturnType:(const char *)returnType argumentTypes:(const char *)firstArgumentType, ...
+{
+    va_list arguments;
+    va_start(arguments, firstArgumentType);
 
-+(const char *)EMK_typesForMethodWithReturnType:(const char *)returnType argumentTypes:(const char *)firstArgumentTypes, ...
+    const char *result = EMK_NSMethodSignature_typesForMethodWithReturnType(returnType, firstArgumentType, arguments);
+
+    va_end(arguments);    
+    
+    return result;
+}
+
+
+
++(NSMethodSignature *)EMK_signatureForMethodWithReturnType:(const char *)returnType argumentTypes:(const char *)firstArgumentType, ...
+{
+    va_list arguments;
+    va_start(arguments, firstArgumentType);
+    const char *types = EMK_NSMethodSignature_typesForMethodWithReturnType(returnType, firstArgumentType, arguments);
+    va_end(arguments);    
+    
+    return [NSMethodSignature signatureWithObjCTypes:types];
+}
+
+@end
+
+
+
+
+const char *EMK_NSMethodSignature_typesForMethodWithReturnType(const char *returnType, const char *firstArgumentType, va_list arguments)
 {
     NSString *compulsoryTypes =  [NSString stringWithFormat: @"%s%s%s", returnType, //return type
                                   @encode(id), //implicit method arg
                                   @encode(SEL)]; //implicit method arg
     
     NSString *types = compulsoryTypes;    
-    const char *argumentType = firstArgumentTypes;    
-    va_list arguments;
-    va_start(arguments, firstArgumentTypes);    
+    const char *argumentType = firstArgumentType;
     
     while (argumentType != NULL)
     {
@@ -32,10 +63,6 @@
         argumentType = va_arg(arguments, const char *);
     }
     
-    va_end(arguments);
-    
+    NSLog(@"%@", types);
     return [types UTF8String];
 }
-
-
-@end
