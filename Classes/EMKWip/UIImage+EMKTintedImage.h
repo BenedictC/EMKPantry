2a125cf2047e5b07263da2a57aee804fafb3a62e
Benedict Cohen <ben@benedictcohen.co.uk>
2011-06-01 09:50:32 +0100

    - Added tinted image

--- Classes/EMKWip/UIImage+EMKTintedImage.h
+++ Classes/EMKWip/UIImage+EMKTintedImage.h
@@ -1,14 +1,16 @@
 //
-//  UIImage+(EMKTintedImage).h
+//  UIImage+EMKTintedImage.h
 //  Jot
 //
 //  Created by Benedict Cohen on 30/05/2011.
 //  Copyright 2011 Electric Muffin Kitchen. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 
-@interface UIImage (UIImage__EMKTintedImage_)
+@interface UIImage (EMKTintedImage)
+
+-(UIImage *)EMK_tintedImageWithColor:(UIColor *)color;
 
 @end
--- Classes/EMKWip/UIImage+EMKTintedImage.m
+++ Classes/EMKWip/UIImage+EMKTintedImage.m
@@ -1,14 +1,45 @@
 //
-//  UIImage+(EMKTintedImage).m
+//  UIImage+EMKTintedImage.m
 //  Jot
 //
 //  Created by Benedict Cohen on 30/05/2011.
 //  Copyright 2011 Electric Muffin Kitchen. All rights reserved.
 //
 
-#import "UIImage+(EMKTintedImage).h"
+#import "UIImage+EMKTintedImage.h"
 
 
-@implementation UIImage (UIImage__EMKTintedImage_)
+@implementation UIImage (EMKTintedImage)
+
+
+-(UIImage *)EMK_tintedImageWithColor:(UIColor *)color
+{
+    //create a new context and set the area to the size of the base image
+    UIGraphicsBeginImageContextWithOptions(self.size, NO, 1);
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+
+    CGRect area = (CGRect){.size = self.size, .origin = CGPointZero};
+    
+    CGContextScaleCTM(ctx, 1, -1);
+    CGContextTranslateCTM(ctx, 0, -area.size.height);            
+
+    //fill the context with tintColor
+    CGContextSaveGState(ctx);
+    CGContextClipToMask(ctx, area, self.CGImage);
+    [color set];
+    CGContextFillRect(ctx, area);
+    CGContextRestoreGState(ctx);
+    
+    //blend baseImage into the ctx
+    CGContextSetBlendMode(ctx, kCGBlendModeColorBurn);
+    CGContextDrawImage(ctx, area, self.CGImage);
+
+
+    //Get the finished image, pop the context and return
+    UIImage *result = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+
+    return result;
+}
 
 @end
